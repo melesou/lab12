c1dedc572649b14ec36885f65fe339ab2c515d9b
melesou <melesou@mail.ru>
2022-05-05 15:48:48 +0500

    Добавьте файлы проекта.

---
+++ lab12/Form1.Designer.cs
@@ -0,0 +1,373 @@
+﻿
+namespace lab12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.очиститьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label25 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Silver;
+            this.label1.Location = new System.Drawing.Point(729, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Палитра";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(726, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(715, 299);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Толщина пера";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(729, 325);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(58, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(815, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.очиститьToolStripMenuItem});
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // очиститьToolStripMenuItem
+            // 
+            this.очиститьToolStripMenuItem.Name = "очиститьToolStripMenuItem";
+            this.очиститьToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.очиститьToolStripMenuItem.Text = "Очистить";
+            this.очиститьToolStripMenuItem.Click += new System.EventHandler(this.очиститьToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(12, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(697, 426);
+            this.panel1.TabIndex = 5;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.down);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.move);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.up);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(726, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Blue;
+            this.label5.Location = new System.Drawing.Point(759, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(759, 81);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(25, 25);
+            this.label7.TabIndex = 9;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.DimGray;
+            this.label8.Location = new System.Drawing.Point(725, 175);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(27, 27);
+            this.label8.TabIndex = 10;
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.DimGray;
+            this.label9.Location = new System.Drawing.Point(758, 143);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(27, 27);
+            this.label9.TabIndex = 10;
+            // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.Color.DimGray;
+            this.label10.Location = new System.Drawing.Point(758, 175);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(27, 27);
+            this.label10.TabIndex = 11;
+            // 
+            // label11
+            // 
+            this.label11.BackColor = System.Drawing.Color.DimGray;
+            this.label11.Location = new System.Drawing.Point(758, 112);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(27, 27);
+            this.label11.TabIndex = 14;
+            // 
+            // label12
+            // 
+            this.label12.BackColor = System.Drawing.Color.DimGray;
+            this.label12.Location = new System.Drawing.Point(758, 80);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(27, 27);
+            this.label12.TabIndex = 12;
+            // 
+            // label13
+            // 
+            this.label13.BackColor = System.Drawing.Color.DimGray;
+            this.label13.Location = new System.Drawing.Point(725, 112);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(27, 27);
+            this.label13.TabIndex = 13;
+            // 
+            // label14
+            // 
+            this.label14.BackColor = System.Drawing.Color.DimGray;
+            this.label14.Location = new System.Drawing.Point(725, 143);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(27, 27);
+            this.label14.TabIndex = 15;
+            // 
+            // label15
+            // 
+            this.label15.BackColor = System.Drawing.Color.DimGray;
+            this.label15.Location = new System.Drawing.Point(725, 80);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(27, 27);
+            this.label15.TabIndex = 16;
+            // 
+            // label16
+            // 
+            this.label16.BackColor = System.Drawing.Color.Silver;
+            this.label16.Location = new System.Drawing.Point(715, 68);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(80, 215);
+            this.label16.TabIndex = 17;
+            // 
+            // label17
+            // 
+            this.label17.BackColor = System.Drawing.Color.Yellow;
+            this.label17.Location = new System.Drawing.Point(726, 144);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(25, 25);
+            this.label17.TabIndex = 18;
+            this.label17.Click += new System.EventHandler(this.label17_Click);
+            // 
+            // label18
+            // 
+            this.label18.BackColor = System.Drawing.Color.Green;
+            this.label18.Location = new System.Drawing.Point(759, 144);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(25, 25);
+            this.label18.TabIndex = 19;
+            this.label18.Click += new System.EventHandler(this.label18_Click);
+            // 
+            // label19
+            // 
+            this.label19.BackColor = System.Drawing.Color.Cyan;
+            this.label19.Location = new System.Drawing.Point(726, 176);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(25, 25);
+            this.label19.TabIndex = 19;
+            this.label19.Click += new System.EventHandler(this.label19_Click);
+            // 
+            // label20
+            // 
+            this.label20.BackColor = System.Drawing.Color.Purple;
+            this.label20.Location = new System.Drawing.Point(759, 176);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(25, 25);
+            this.label20.TabIndex = 19;
+            this.label20.Click += new System.EventHandler(this.label20_Click);
+            // 
+            // label25
+            // 
+            this.label25.BackColor = System.Drawing.Color.DimGray;
+            this.label25.Location = new System.Drawing.Point(725, 213);
+            this.label25.Name = "label25";
+            this.label25.Size = new System.Drawing.Size(60, 60);
+            this.label25.TabIndex = 24;
+            // 
+            // label21
+            // 
+            this.label21.BackColor = System.Drawing.Color.DimGray;
+            this.label21.Location = new System.Drawing.Point(726, 214);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(58, 58);
+            this.label21.TabIndex = 25;
+            this.label21.Click += new System.EventHandler(this.label21_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(815, 465);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.label25);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label16);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem очиститьToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label25;
+        private System.Windows.Forms.Label label21;
+    }
+}
+
---
+++ lab12/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label2.BackColor = Color.Black; 
+            g = panel1.CreateGraphics();
+        }
+
+        Color color = Color.Black; 
+        bool isPressed = false; 
+        Point CurrentPoint; 
+        Point PrevPoint; 
+        Graphics g; 
+        ColorDialog colorDialog = new ColorDialog();
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            colorDialog.Color = label2.BackColor;
+            color = colorDialog.Color;
+        }
+
+        private void my_Pen()
+        {
+            Pen pen = new Pen(color, (float)numericUpDown1.Value);
+            g.DrawLine(pen, CurrentPoint, PrevPoint);
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void down(object sender, MouseEventArgs e)
+        {
+            isPressed = true;
+            CurrentPoint = e.Location;
+        }
+
+        private void up(object sender, MouseEventArgs e)
+        {
+            isPressed = false;
+        }
+
+        private void move(object sender, MouseEventArgs e)
+        {
+            if (isPressed)
+            {
+                PrevPoint = CurrentPoint;
+                CurrentPoint = e.Location;
+                my_Pen();
+            }
+        }
+
+        private void очиститьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            panel1.Refresh();           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+            colorDialog.Color = label7.BackColor;
+            color = colorDialog.Color;
+            
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            colorDialog.Color = label4.BackColor;
+            color = colorDialog.Color;
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            colorDialog.Color = label5.BackColor;
+            color = colorDialog.Color;
+        }
+
+        private void label17_Click(object sender, EventArgs e)
+        {
+            colorDialog.Color = label17.BackColor;
+            color = colorDialog.Color;
+        }
+
+        private void label18_Click(object sender, EventArgs e)
+        {
+            colorDialog.Color = label18.BackColor;
+            color = colorDialog.Color;
+        }
+
+        private void label19_Click(object sender, EventArgs e)
+        {
+            colorDialog.Color = label19.BackColor;
+            color = colorDialog.Color;
+        }
+
+        private void label20_Click(object sender, EventArgs e)
+        {
+            colorDialog.Color = label20.BackColor;
+            color = colorDialog.Color;
+        }
+
+        private void label21_Click(object sender, EventArgs e)
+        {
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                color = colorDialog.Color;
+                label21.BackColor = colorDialog.Color;
+            }
+        }
+    }
+}
